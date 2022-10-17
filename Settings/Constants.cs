ca1d6bdc40a282709f37786b4d601f149fb6659f
Marcel Greter <marcel.greter@ocbnet.ch>
2022-10-17 18:20:29 +0200

    Prograss

---
+++ Settings/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeManager
+{
+
+    public enum TYPES : ushort
+    {
+        PipeConnection = 1,
+        PipePump = 2,
+        PipeIrrigation = 3,
+        PipeSouce = 4,
+        PipeWaterBoiler = 5,
+        PipeFluidConverter = 6,
+
+        PipeReservoir = 7,
+        PipeFluidInjector = 13,
+        PlantationSprinkler = 14,
+        PipeWell = 9,
+        PlantationFarmLand = 8,
+        PlantationFarmPlot = 10,
+        PlantationGrowing = 11,
+
+        PlantationComposter = 12,
+    }
+
+}
